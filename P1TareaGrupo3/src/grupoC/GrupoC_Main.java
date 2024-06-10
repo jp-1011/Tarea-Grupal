b0d6650a81e8fbc472006645eab113e595a6a038
jp-1011 <magikking2004@hotmail.com>
2024-06-09 19:20:35 -0500

    Update GrupoC_Main.java

--- P1TareaGrupo3/src/grupoC/GrupoC_Main.java
+++ P1TareaGrupo3/src/grupoC/GrupoC_Main.java
@@ -1,7 +1,8 @@
 package grupoC;
 public class GrupoC_Main {
 	public static void main(String[] args) {
-		GrupoC_Store menu = new GrupoC_Store(0);
+	    GrupoC_Store menu = new GrupoC_Store(0, " ");
 		menu.menuOptions();
 	}
 }
+
